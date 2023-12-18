34cd0fbdc372e97c7f0fccf6e312d920231908af
Naturelitetech <nlf.tech.2023@gmail.com>
2023-12-18 20:46:14 +0530

    all functionality completed

--- modules/auth/authController.js
+++ modules/auth/authController.js
@@ -1,48 +1,51 @@
 // auth/authController.js
 const db = require('../../config/db');
 const { generateToken } = require('../../utils/helpers');
+//const bcrypt = require('bcrypt');
 
 const authController = {
 
     signup: async (req, res) => {
+        try {
+            const name = req.body.name;
+            const number = req.body.number;
+            const password = req.body.password;
+            const department = req.body.department;
+            const role = req.body.role;
+            const email = 'ashi133@gmail.com'; // Fixed email
+            const verificationToken = generateToken();
 
-        const name = req.body.name;
-        const number = req.body.number;
-        const password = req.body.password;
-        const department = req.body.department;
-        const role = req.body.role;
-        const email = 'ashi133@gmail.com'; // Fixed email
-        const verificationToken = generateToken();
-
-        const query = 'SELECT role_id FROM role WHERE role_name = ?';
-
-        db.query(query, [role], (roleErr, roleResults) => {
-            if (roleErr) {
-                console.error('Error executing role query: ' + roleErr);
-                res.status(500).json({ error: 'Database error' });
-                return;
-            }
-
-            if (roleResults.length === 0) {
-                res.status(404).json({ error: 'Role not found' });
-                return;
-            }
-
-            const roleId = roleResults[0].role_id;
-
-            // insert the user data and role_id into the database
-            const sql = 'INSERT INTO user (name, number, password, department, role_id, email, verification_token) VALUES (?, ?, ?, ?, ?, ?, ?)';
-            db.query(sql, [name, number, password, department, roleId, email, verificationToken], (err, data) => {
-                if (err) {
-                    console.error('Error executing user query: ' + err);
+            const query = 'SELECT role_id FROM role WHERE role_name = ?';
+            db.query(query, [role], (roleErr, roleResults) => {
+                if (roleErr) {
+                    console.error('Error executing role query: ' + roleErr);
                     res.status(500).json({ error: 'Database error' });
+                    return;
                 }
-                else {
-                    res.json({ message: 'User registered successfully' });
-                }
-            });
-        });
 
+                if (roleResults.length === 0) {
+                    res.status(404).json({ error: 'Role not found' });
+                    return;
+                }
+
+                const roleId = roleResults[0].role_id;
+
+                // insert the user data and role_id into the database
+                const sql = 'INSERT INTO user (name, number, password, department, role_id, email, verification_token) VALUES (?, ?, ?, ?, ?, ?, ?)';
+                db.query(sql, [name, number, password, department, roleId, email, verificationToken], (err, data) => {
+                    if (err) {
+                        console.error('Error executing user query: ' + err);
+                        res.status(500).json({ error: 'Database error' });
+                    }
+                    else {
+                        res.json({ message: 'User registered successfully' });
+                    }
+                });
+            });
+        } catch (error) {
+            console.error('Error during signup:', error);
+            res.status(500).json({ error: 'Server error during signup' });
+        }
     },
 
     login: (req, res) => {
