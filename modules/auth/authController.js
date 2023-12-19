5f2253a4a7cc03947c417d067a2cd489b1dd9533
Naturelitetech <nlf.tech.2023@gmail.com>
2023-12-19 20:06:32 +0530

    password is bcrypt

--- modules/auth/authController.js
+++ modules/auth/authController.js
@@ -1,7 +1,7 @@
 // auth/authController.js
 const db = require('../../config/db');
 const { generateToken } = require('../../utils/helpers');
-//const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 const authController = {
 
@@ -9,14 +9,17 @@ const authController = {
         try {
             const name = req.body.name;
             const number = req.body.number;
-            const password = req.body.password;
+            const password = String(req.body.password);
             const department = req.body.department;
             const role = req.body.role;
             const email = 'ashi133@gmail.com'; // Fixed email
             const verificationToken = generateToken();
 
+            // Hash the password before storing it in the database
+            const hashedPassword = await bcrypt.hash(password, 10);
+
             const query = 'SELECT role_id FROM role WHERE role_name = ?';
-            db.query(query, [role], (roleErr, roleResults) => {
+            db.query(query, [role], async (roleErr, roleResults) => {
                 if (roleErr) {
                     console.error('Error executing role query: ' + roleErr);
                     res.status(500).json({ error: 'Database error' });
@@ -32,7 +35,7 @@ const authController = {
 
                 // insert the user data and role_id into the database
                 const sql = 'INSERT INTO user (name, number, password, department, role_id, email, verification_token) VALUES (?, ?, ?, ?, ?, ?, ?)';
-                db.query(sql, [name, number, password, department, roleId, email, verificationToken], (err, data) => {
+                db.query(sql, [name, number, hashedPassword, department, roleId, email, verificationToken], (err, data) => {
                     if (err) {
                         console.error('Error executing user query: ' + err);
                         res.status(500).json({ error: 'Database error' });
@@ -53,27 +56,42 @@ const authController = {
         const { number, password } = req.body;
 
         // Verify user credentials here
-        db.query('SELECT * FROM user WHERE number = ? AND password = ?', [number, password], (err, results) => {
+        db.query('SELECT * FROM user WHERE number = ?', [number], async (err, results) => {
             if (err) {
                 console.error('Error during login: ' + err);
                 res.status(500).send('Error during login');
             } else if (results.length > 0) {
-                // User exists and credentials match
-                const userId = results[0].user_id;
-                const verified = results[0].verified;
+                // User exists
+                const hashedPasswordFromDatabase = results[0].password;
 
-                // Check the user's verification status
-                db.query('SELECT verified FROM user WHERE user_id = ?', [userId], (err, results) => {
-                    if (err) {
-                        console.error('Error checking user verification status: ' + err);
-                        res.status(500).send('Error checking user verification status');
-                    } else if (results.length > 0 && results[0].verified === 1) {
-                        // res.status(200).send('Login successful');
-                        res.status(200).json({ userId }); // Include userId in the response
-                    } else {
-                        res.status(401).send('Please wait for verification');
-                    }
-                });
+                // Hash the provided password for comparison
+                //const hashedProvidedPassword = await bcrypt.hash(password, 10);
+
+                // Compare the plaintext password with the hashed password from the database
+                const passwordMatch = await bcrypt.compare(password, hashedPasswordFromDatabase);
+
+                if (passwordMatch) {
+                    // User exists and credentials match
+                    const userId = results[0].user_id;
+                    const verified = results[0].verified;
+
+                    // Check the user's verification status
+                    db.query('SELECT verified FROM user WHERE user_id = ?', [userId], (err, results) => {
+                        if (err) {
+                            console.error('Error checking user verification status: ' + err);
+                            res.status(500).send('Error checking user verification status');
+                        } else if (results.length > 0 && results[0].verified === 1) {
+                            // res.status(200).send('Login successful');
+                            res.status(200).json({ userId }); // Include userId in the response
+                        } else {
+                            res.status(401).send('Please wait for verification');
+                        }
+                    });
+                } else {
+                    // Passwords don't match
+                    console.log('Passwords do not match');
+                    res.status(401).send('Incorrect credentials');
+                }
             } else {
                 // User not found or credentials don't match
                 res.status(401).send('No record exists');
@@ -81,6 +99,5 @@ const authController = {
         });
 
     },
-
 }
 module.exports = authController;
